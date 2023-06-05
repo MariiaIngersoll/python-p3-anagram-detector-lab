1cfb3b0e6aa65698eb008f26261edd401fe267e9
+ <+>
2023-06-04 20:47:21 -0400

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        new_list = []
+        for letter in list:
+            if sorted(letter.lower()) == sorted(self.word.lower()):
+                new_list.append(letter)
+            else:
+                return new_list
